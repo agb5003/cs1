63aa045f397ccdf408e2db7229e9589564464f58
Maximilian Fernaldy <maximilian.fernaldy@gmail.com>
2024-01-22 17:12:59 +0900

    decorators

--- projects/particle/particle.py
+++ projects/particle/particle.py
@@ -5,24 +5,34 @@ class World:
         self.width = width
         self.height = height
         self.dt = dt
-        self.gy = gy
+        self.gravity_acc = pygame.math.Vector2(0, gy)
 
 class Particle:
     def __init__(self, pos, vel, world, radius=10.0, color = "green"):
         self.is_alive = True
-        self.x, self.y = pos
-        self.vx, self.vy = vel
+        self.pos = pygame.math.Vector2(pos)
+        self.vel = pygame.math.Vector2(vel)
         self.world = world
         self.radius = radius
         self.color = pygame.Color(color)
 
     def update(self):
-        self.vy += self.world.gy * self.world.dt
-        self.x += self.vx * self.world.dt
-        self.y += self.vy * self.world.dt
+        self.vel += self.world.gravity_acc * self.world.dt
+        self.pos += self.vel * self.world.dt
+        self.update_after_move()
+
+    def update_after_move(self):
         if self.x < 0 or self.x > self.world.width or self.y > self.world.height:
             self.is_alive = False
 
     def draw(self, screen):
         radius = 10
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+    
+    @property
+    def x(self):
+        return self.pos.x
+    
+    @property
+    def y(self):
+        return self.pos.y
